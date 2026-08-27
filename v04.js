2cfc81853b1dbbbd6b10c005dda32a78daddba33
Pablo-Morrondo <morrondin@gmail.com>
2026-08-27 22:01:49 +0200

    Cargar capa v0.5

--- v04.js
+++ v04.js
@@ -1,5 +1,8 @@
 (() => {
   'use strict';
   const load=src=>new Promise((resolve,reject)=>{const s=document.createElement('script');s.src=src;s.onload=resolve;s.onerror=reject;document.head.appendChild(s)});
-  load('./v042-base.js?v=042').then(()=>load('./v043.js?v=043')).catch(err=>console.error('No se pudo cargar v0.4.3',err));
+  load('./v042-base.js?v=042')
+    .then(()=>load('./v043.js?v=043'))
+    .then(()=>load('./v05.js?v=050'))
+    .catch(err=>console.error('No se pudo cargar v0.5',err));
 })();
