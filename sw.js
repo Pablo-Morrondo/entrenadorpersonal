e341ebc523920293fc2cff404de1f978831b165a
Pablo-Morrondo <morrondin@gmail.com>
2026-08-28 17:42:48 +0200

    Actualizar caché a v0.5.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='entrenador-personal-v0.5.4',A=['./','./index.html','./v032.js','./v04.js?v=054','./v042-base.js?v=042','./v043.js?v=043','./v05.js?v=050','./v051.js?v=051','./v052.js?v=053','./v054.js?v=054','./manifest.webmanifest','./icon.svg'];
+const C='entrenador-personal-v0.5.5',A=['./','./index.html','./v032.js','./v04.js?v=055','./v042-base.js?v=042','./v043.js?v=043','./v05.js?v=050','./v051.js?v=051','./v052.js?v=053','./v054.js?v=054','./v055.js?v=055','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
