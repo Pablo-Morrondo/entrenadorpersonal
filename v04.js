7adffce209de7d3abef70975cb066d7893be88e3
Pablo-Morrondo <morrondin@gmail.com>
2026-08-29 21:32:42 +0200

    Load v0.6.3 workout start fix

--- v04.js
+++ v04.js
@@ -15,5 +15,6 @@
     .then(()=>load('./v060.js?v=060'))
     .then(()=>load('./v061.js?v=061'))
     .then(()=>load('./v062.js?v=062'))
+    .then(()=>load('./v063.js?v=063'))
     .catch(err=>console.error('No se pudo cargar v0.6',err));
 })();
