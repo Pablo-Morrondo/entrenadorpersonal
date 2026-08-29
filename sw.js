5d66bccc3d7c9b177695cc573f2a5d1731dd66f9
Pablo-Morrondo <morrondin@gmail.com>
2026-08-29 21:28:30 +0200

    Cache v0.6.2 workout state flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='entrenador-personal-v0.6.1',A=['./','./index.html','./v032.js','./v04.js?v=061','./v042-base.js?v=042','./v043.js?v=043','./v05.js?v=050','./v051.js?v=051','./v052.js?v=053','./v054.js?v=054','./v055.js?v=055','./v056.js?v=056','./v057.js?v=057','./v058.js?v=058','./v059.js?v=059','./v060.js?v=060','./v061.js?v=061','./manifest.webmanifest','./icon.svg'];
+const C='entrenador-personal-v0.6.2',A=['./','./index.html','./v032.js','./v04.js?v=062','./v042-base.js?v=042','./v043.js?v=043','./v05.js?v=050','./v051.js?v=051','./v052.js?v=053','./v054.js?v=054','./v055.js?v=055','./v056.js?v=056','./v057.js?v=057','./v058.js?v=058','./v059.js?v=059','./v060.js?v=060','./v061.js?v=061','./v062.js?v=062','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
