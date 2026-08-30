2e32f5e1d6204e99b62314b89fc7a7465c7a7cce
Pablo-Morrondo <morrondin@gmail.com>
2026-08-30 20:27:03 +0200

    Load v0.7.0 chat fallback

--- v04.js
+++ v04.js
@@ -17,5 +17,6 @@
     .then(()=>load('./v066.js?v=066'))
     .then(()=>load('./v068.js?v=068'))
     .then(()=>load('./v069.js?v=069'))
-    .catch(err=>console.error('No se pudo cargar v0.6',err));
+    .then(()=>load('./v070.js?v=070'))
+    .catch(err=>console.error('No se pudo cargar v0.7',err));
 })();
